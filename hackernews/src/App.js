9ed7bed819a0f254433b3a14255b4bcc2b9461e3
pauantonio <pauantoniosoler@gmail.com>
2022-05-25 10:21:21 +0200

    user submissions

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,6 +3,7 @@ import News from "./screens/NewsScreen";
 import User from "./screens/UserScreen";
 import SubmitForm from "./screens/SubmitScreen";
 import UpvotedSubmissions from "./screens/UpvotedSubmissionsScreen";
+import UserSubmissions from "./screens/UserSubmissionsScreen";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="/user" element={<User />} />
 				<Route path="/submit" element={<SubmitForm />} />
 				<Route path="/upvotedSubmissions" element={<UpvotedSubmissions />} />
+				<Route path="/user/submissions" element={<UserSubmissions />} />
 			</Routes>
 		</div>
 	);
